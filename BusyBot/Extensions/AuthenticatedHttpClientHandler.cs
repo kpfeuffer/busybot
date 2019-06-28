67b597c6e3720f009ba71879f562300f025a9e11
kpfeuffer <kpfeuffer@gitlab.com>
2019-06-28 16:47:15 +0200

    Added some Extensions for Utility Usage

---
+++ BusyBot/Extensions/AuthenticatedHttpClientHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BusyBot.Extensions
+{
+    public class AuthenticatedHttpClientHandler: HttpClientHandler
+    {
+        private readonly string _token;
+
+        public AuthenticatedHttpClientHandler(string token)
+        {
+            _token = token ?? throw new ArgumentNullException(nameof(token));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // See if the request has an authorize header
+            var auth = request.Headers.Authorization;
+            if (auth != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue(auth.Scheme, _token);
+            }
+
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+
+    }
+}
