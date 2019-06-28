8945dcb47bc6733154aecc426e4e9c24e3a84c10
kpfeuffer <kpfeuffer@gitlab.com>
2019-06-28 16:51:38 +0200

    Added Bot Service, using Readers and Template Service to get Formatted Data as Bot response

---
+++ BusyBot/Services/Contracts/IBotService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BusyBot.Services
+{
+    public interface IBotService
+    {
+        Task HandleMessage(Activity activity, CancellationToken cancellationToken = default);
+
+        Task<bool> HandleAdaptiveCardAction(Activity activity, CancellationToken cancellation = default);
+    }
+}
---
+++ BusyBot/Services/Implementations/BotService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using AdaptiveCards;
+using BusyBot.Commands;
+using BusyBot.Domain.Readers;
+using BusyBot.Models.Commands;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace BusyBot.Services
+{
+    public class BotService : IBotService
+    {
+        private readonly IQueryCommands queryCommands;
+        private readonly ITemplateService templateService;
+        private readonly IEventReader eventReader;
+        public BotService(
+            IQueryCommands queryCommands,
+            ITemplateService templateService,
+            IEventReader eventReader
+            )
+        {
+            this.queryCommands = queryCommands;
+            this.templateService = templateService;
+            this.eventReader = eventReader;
+        }
+
+        public async Task<bool> HandleAdaptiveCardAction(Activity activity, CancellationToken cancellation = default)
+        {
+            var command = JsonConvert.DeserializeObject<ActionCommandBase>(activity.Value?.ToString());
+            if (string.IsNullOrEmpty(command?.CommandId))
+            {
+                return false;
+            }
+
+            IEnumerable<AdaptiveCard> results = null;
+            if(command.CommandId == queryCommands.GetEvents) 
+            {
+                var events = await this.eventReader.GetCurrentEvents((command as GetCurrentEventsCommand).UserId);
+                results = this.templateService.CurrentEvents(events);
+            } 
+
+            if(results is null)
+            {
+                return false;
+            }
+
+            var message = Activity.CreateMessageActivity();
+            var client = new ConnectorClient(new Uri(activity.ServiceUrl));
+            foreach(AdaptiveCard card in results)
+            {
+                message.Attachments.Add(new Attachment
+                {
+                    ContentType = AdaptiveCard.ContentType,
+                    Content = card
+                });
+            }
+            await client.Conversations.UpdateActivityAsync(activity.Conversation.Id, activity.ReplyToId, (Activity) message, cancellation);
+
+            return true;
+        }
+
+        public Task HandleMessage(Activity activity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
