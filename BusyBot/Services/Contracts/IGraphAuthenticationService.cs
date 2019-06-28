3a620653aa42cba69ef769ad3a5120948b786de5
kpfeuffer <kpfeuffer@gitlab.com>
2019-06-28 16:49:25 +0200

    Added Graph Authentication Service with blank Implementation

---
+++ BusyBot/Services/Contracts/IGraphAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusyBot.Services
+{
+    public interface IGraphAuthenticationService
+    {
+        string GetAccessToken();
+    }
+}
---
+++ BusyBot/Services/Implementations/GraphAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusyBot.Services
+{
+    public class GraphAuthenticationService : IGraphAuthenticationService
+    {
+        public string GetAccessToken()
+        {
+            return string.Empty;
+        }
+    }
+}
