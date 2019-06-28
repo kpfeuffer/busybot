6f1dd977a54be4a542823d8de681372b5f9f2e30
kpfeuffer <kpfeuffer@gitlab.com>
2019-06-28 16:53:11 +0200

    Registered Dependencies in Module

--- BusyBot/App_Start/BusyBotAppModule.cs
+++ BusyBot/App_Start/BusyBotAppModule.cs
@@ -1,4 +1,10 @@
 ﻿using Autofac;
+using BusyBot.Commands;
+using BusyBot.Domain.Readers;
+using BusyBot.Extensions;
+using BusyBot.Repositories;
+using BusyBot.Services;
+using Refit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +18,16 @@ namespace BusyBot
         {
             base.Load(builder);
 
-            
+            builder.RegisterType<QueryCommands>().As<IQueryCommands>();
+
+            builder.RegisterType<GraphEventsRepository>().As<IGraphEventsRepository>();
+            builder.RegisterType<GraphUserRepository>().As<IGraphUserRepository>();
+
+            builder.RegisterType<EventReader>().As<IEventReader>();
+            builder.RegisterType<UserReader>().As<IUserReader>();
+            builder.RegisterType<BotService>().As<IBotService>();
+            builder.RegisterType<TemplateService>().As<ITemplateService>();
+                       
         }
     }
 }
