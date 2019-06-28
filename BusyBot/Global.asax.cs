86baefe827be8e3ac4118647a37e20c93270d7c9
kpfeuffer <kpfeuffer@gitlab.com>
2019-06-28 16:53:37 +0200

    Updated Project-Registrations and Registrations in asax.cs

--- BusyBot/Global.asax.cs
+++ BusyBot/Global.asax.cs
@@ -1,8 +1,13 @@
 using Autofac;
+using Autofac.Integration.WebApi;
 using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -17,6 +22,7 @@ namespace BusyBot
         {
             var config = GlobalConfiguration.Configuration;
             ConfigureServices(config);
+            Configure(config);
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
@@ -29,7 +35,24 @@ namespace BusyBot
                 {
                     builder.RegisterModule(new BusyBotAppModule());
 
+                    builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+                    builder.RegisterWebApiFilterProvider(config);
                 });
+        }
+
+        private static void Configure(HttpConfiguration config)
+        {
+            // Json settings
+            var jsonSerializerSettings = config.Formatters.JsonFormatter.SerializerSettings;
+            jsonSerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            jsonSerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonSerializerSettings.Converters.Add(new StringEnumConverter
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            });
+            jsonSerializerSettings.Formatting = Formatting.Indented;
+            JsonConvert.DefaultSettings = () => jsonSerializerSettings;
+
         }
     }
 }
