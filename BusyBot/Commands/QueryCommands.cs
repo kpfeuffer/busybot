365e3c027c1678279df7de2cd076ac64a1ce82c3
kpfeuffer <kpfeuffer@gitlab.com>
2019-06-28 16:46:43 +0200

    Added Query Commands for Determining User Intens when getting Messages

---
+++ BusyBot/Commands/QueryCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusyBot.Commands
+{
+    public class QueryCommands : IQueryCommands
+    {
+        public string GetEvents { get; } = "getEvents";
+
+        public string FindUsers { get; } = "findUsers";
+    }
+
+    public interface IQueryCommands
+    {
+        string GetEvents { get; }
+        string FindUsers {get;}
+    }
+}
