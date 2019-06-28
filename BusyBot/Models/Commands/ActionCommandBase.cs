3c67e9d4955275e5aa75abe02fb3dc305a700100
kpfeuffer <kpfeuffer@gitlab.com>
2019-06-28 16:44:47 +0200

    Added Input-Actions as Commands

---
+++ BusyBot/Models/Commands/ActionCommandBase.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusyBot.Models.Commands
+{
+    public class ActionCommandBase
+    {
+        [JsonProperty("commandId")]
+        public string CommandId { get; set; }
+    }
+}
---
+++ BusyBot/Models/Commands/GetCurrentEventsCommand.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusyBot.Models.Commands
+{
+    public class GetCurrentEventsCommand: ActionCommandBase
+    {
+        [JsonProperty("userId")]
+        public string UserId{ get; set; }
+    }
+}
